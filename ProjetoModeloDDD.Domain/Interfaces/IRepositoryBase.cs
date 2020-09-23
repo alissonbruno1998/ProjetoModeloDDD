09b9e6769b8a65b03245d95610a26e21a436dcde
Alisson Bruno <alissonbruno.2015@hotmail.com>
2020-09-22 21:21:25 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoModeloDDD.Domain/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Domain.Interfaces
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+
+        TEntity GetEntity(int id);
+
+        IEnumerable<TEntity> GetAll();
+
+        void Update(TEntity obj);
+
+        void Remove(TEntity obj);
+
+        void Dispose();
+         
+    }
+}
